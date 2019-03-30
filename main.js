7298bd45915c59fff057857f62847e2f69c5d673
Keith Walker <Keithrwalker418@gmail.com>
2019-03-30 15:10:48 -0500

    fixed problems with JS, and linked the navBar to every page

--- main.js
+++ main.js
@@ -113,30 +113,36 @@ const beerCardBuilder = (arrayToPrint) => {
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
 
-const printNavBar = () => {
-    let domString = ``;
-    domString +=
-    `<nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                <a class="nav-item nav-link" href="#">Features</a>
-                <a class="nav-item nav-link" href="#">Pricing</a>
-                <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </div>
-        </div>
-    </nav>`
-    printToDom('navBar', domString);
-};
+// const printNavBar = () => {
+
+//     let domString = ``;
+//     domString +=
+//     `<nav class="navbar navbar-expand-lg navbar-light bg-light">
+//         <a class="navbar-brand" href="#">Navbar</a>
+//         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+//             <span class="navbar-toggler-icon"></span>
+//         </button>
+//         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+//             <div class="navbar-nav">
+//                 <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+//                 <a class="nav-item nav-link" href="#">Features</a>
+//                 <a class="nav-item nav-link" href="#">Pricing</a>
+//                 <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+//             </div>
+//         </div>
+//     </nav>`
+//     printToDom('navBar', domString);
+// };
 
 const printMap = () => {
+    const mapContainer = document.getElementById('mapContainer');
+    if(mapContainer === null){
+        return;
+    } else{
     let domString = ``;
     domString += `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3222.393140738528!2d-86.75879268470213!3d36.13263981268999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88646609ffab37eb%3A0x528b2eab4ef5110e!2s500+Interstate+Blvd+S%2C+Nashville%2C+TN+37210!5e0!3m2!1sen!2sus!4v1553963381330!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>`;
     printToDom('mapContainer', domString);
+    };
 };
 
 const shoppingCartArray = [
@@ -174,6 +180,10 @@ const shoppingCartArray = [
 ];
 
 const printCart = () =>{
+    const checkoutPage = document.getElementById('checkout-card-container');
+    if(checkoutPage===null){
+        return
+    } else{
     let domString = '';
     shoppingCartArray.forEach((beer)=> {
         //if(beer.quantity >= 1){
@@ -191,12 +201,13 @@ const printCart = () =>{
           
         //};
     });
-    printToDom('productCon', domString);
+    printToDom('checkout-card-container', domString);
+}
 };
 
 const init = () =>{
     printCart();
-    printNavBar();
+    // printNavBar();
     printMap();
     beerCardBuilder(beerArray);
 };
