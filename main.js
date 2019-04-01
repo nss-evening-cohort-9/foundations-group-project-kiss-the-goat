faf803e2a02fd83a5957671b33421814da75c9e9
Emily DeWitt <emilykdewitt@gmail.com>
2019-04-01 18:56:22 -0500

    Updated all docs with current code

--- main.js
+++ main.js
@@ -1,102 +1,117 @@
+const brewers = [
+    {
+        name: 'Emily DeWitt',
+        photo: `http://www.ieeeaustsb.org/files/2017/05/placeholder-male-square.png`,
+        hometown: `Hartwell, Georgia`,
+        favoriteBeers: ['Wiseacre Tiny Bomb', 'Red Stripe', 'Tecate'],
+    },
+    {
+        name: 'Josh Pantana',
+        photo: `http://www.ieeeaustsb.org/files/2017/05/placeholder-male-square.png`,
+        hometown: `Atlanta, Georgia`,
+        favoriteBeers: ['Miller Lite', 'Old Speckled Hen', 'Dos Equis'],
+    },
+    {
+        name: 'Keith Walker',
+        photo: `http://www.ieeeaustsb.org/files/2017/05/placeholder-male-square.png`,
+        hometown: `Chicago, Illinois`,
+        favoriteBeers: ['Fullers ESB', 'Budweiser', 'Newcastle Brown Ale'],
+    },
+    {
+        name: 'Austin Casey',
+        photo: `http://www.ieeeaustsb.org/files/2017/05/placeholder-male-square.png`,
+        hometown: `Nashville, Tennessee`,
+        favoriteBeers: ['Chicken Scratch', 'Peanut Butter Milk Stout', 'Jackalope'],
+    },
+];
+//Standard printToDom function everyone uses
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
   };
 
+//This function prints the map showing the location of the brewery to the DOM of the Taproom page
 const printMap = () => {
-    if (document.body.id != 'taproom') {
-        return
+    const mapContainer = document.getElementById('mapContainer');
+    if(mapContainer === null){
+        return;
     } else {
     let domString = ``;
     domString += `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3222.393140738528!2d-86.75879268470213!3d36.13263981268999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88646609ffab37eb%3A0x528b2eab4ef5110e!2s500+Interstate+Blvd+S%2C+Nashville%2C+TN+37210!5e0!3m2!1sen!2sus!4v1553963381330!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>`;
     printToDom('mapContainer', domString);
+    };
+};
+
+//This function prints the address and contact information of the brewery beside the map on the taproom key
+const printContactInfo = () => {
+    const contactInfoContainer = document.getElementById('contactInfoContainer');
+    if (contactInfoContainer === null) {
+        return;
+    } else {
+        let domString = ``;
+        domString += `<div id="contactText">`
+        domString +=    `<h2>Visit our Brewery</h2>`
+        domString +=    `<p>Kiss the Goat Brewery</p>`;
+        domString +=    `<p>500 Interstate Blvd South</p>`;
+        domString +=    `<p>Nashville, Tennessee 37210</p>`;
+        domString +=    `<p>Phone: (615) 736-0000</p>`;
+        domString +=    `<button id="scheduleTour">Click here to schedule a tour</button>`
+        domString += `</div>`
+
+        printToDom('contactInfoContainer', domString)
     }
 };
 
-const printContactInfo = () => {
-    if (document.body.id != 'taproom') {
+//This function prints 'Meet Our Brewers' above the photos of brewers on the taproom page
+const printBrewerTitle = () => {
+    const brewerTitleContainer = document.getElementById('brewerTitleContainer');
+    if (brewerTitleContainer === null) {
         return
     } else {
     let domString = ``;
-    domString += `<div class="infoContainer">`
-    domString +=    `<h3>Stop by for a visit!</h3>`;
-    domString +=    `<h5>Kiss the Goat Brewery</h5>`;
-    domString +=    `<p>500 Interstate Blvd South</p>`;
-    domString +=    `<p>Nashville, Tennessee 37210</p>`;
-    domString +=    `<p>(615)736-0000</p>`;
-    domString +=    `<p>Email us to request a tour!</p>`;
-    domString += `</div>`
-    printToDom('contactInfoContainer', domString);
+    domString += `<h2 id="meetTheBrewersTitle">Meet our Brewers</h2>`;
+    printToDom('brewerTitleContainer', domString);
     }
 };
 
-// //WHEN the 'Taproom' page loads
-// //THEN the address, hours, phone number, and a link to schedule a brewery tour will be displayed to the right of the dynamic map
-// //THEN when I click the 'Schedule a tour' link, a prompt appears that says 'Your request for a tour has been sent!'
-
-const shoppingCartArray = [
-    {
-        name: 'Beer 1 name',
-        imgUrl: 'https://via.placeholder.com/148',
-        pricePer: 1.99,
-        quantity: 1,
-
-    },
-    {
-        name: 'Beer 2 name',
-        imgUrl: 'https://via.placeholder.com/148',
-        pricePer: 2.99,
-        quantity: 2,
-    },
-    {
-        name: 'Beer 3 name',
-        imgUrl: 'https://via.placeholder.com/148',
-        pricePer: 3.99,
-        quantity: 3,
-    },
-    {
-        name: 'Beer 4 name',
-        imgUrl: 'https://via.placeholder.com/148',
-        pricePer: 4.99,
-        quantity: 4,
-    },
-    {
-        name: 'Beer 5 name',
-        imgUrl: 'https://via.placeholder.com/148',
-        pricePer: 5.99,
-        quantity: 5,
-    }
-];
-
-const printCart = () =>{
-    if (document.body.id != 'checkoutPage') {
+//this function prints the photos of the brewers on the Taproom page
+const printBrewerPhotos = () => {
+    const brewerCardContainer = document.getElementById('brewerCardContainer');
+    if (brewerCardContainer === null) {
         return
     } else {
-    let domString = '';
-    shoppingCartArray.forEach((beer)=> {
-        //if(beer.quantity >= 1){
-            domString += `<div class="col-sm-6">`;
-            domString += `  <div class="card">`;
-            domString += `     <div class="card-body">`;
-            domString += `      <img class='card-img-top' src=${beer.imgUrl}>`;
-            domString += `      <p class="card-text">${beer.name}</p>`;
-            domString += `      <p class="card-text">$${beer.pricePer} per pack.</p>`;
-            domString += `      <p class="card-text">Current quantity: ${beer.quantity} packs</p>`;
-            domString += `      <a href="#" class="btn btn-primary">Change</a>`;
-            domString += `     </div>`;
-            domString += `  </div>`;
-            domString += `</div>`;
-          
-        //};
-    });
-    printToDom('checkout-card-container', domString);
-    }
+    let domString = ``;
+    domString += '<div class="row">';
+    brewers.forEach((brewer) => {
+        domString += `<div class="card text-center col-6 brewerCard">`; //CARD
+        domString +=   `<div class="brewerPhotoAndDescriptionWrap">`; //PHOTO AND DESCRIPTION WRAP
+        domString +=     `<img class="brewerPhotoWrap" src="${brewer.photo}">`; //BREWER PHOTO
+        domString +=     `<div class="brewerDescriptionWrap">`; //DESCRIPTION WRAP
+        domString +=       `<h4>${brewer.name}</h4>`;
+        domString +=       `<h5>Hometown: ${brewer.hometown}</h5>`;
+        domString +=       `<h5>Favorite beers: ${brewer.favoriteBeers}</h5>`;
+        domString +=     `</div>`; //END DESCRIPTION WRAP
+        domString +=   `</div>` //END PHOTO AND DESCRIPTION WRAP
+        domString +=  `</div>`; //END CARD
+        printToDom('brewerCardContainer', domString);
+      });
 };
 
-const init = () =>{
-    printCart();
+//This causes an alert to appear when someone clicks the 'Schedule a Tour' button on the Taproom page
+const scheduleAlert = () => {
+    alert("Your request to schedule a brewery tour has been received. We'll be in touch shortly!");
+};
+
+const eventListeners = () => {
+    document.getElementById('scheduleTour').addEventListener('click', scheduleAlert);
+}
+
+const init = () => {
     printMap();
     printContactInfo();
+    printBrewerTitle();
+    printBrewerPhotos();
+    eventListeners();
 };
 
 init();
